d37ac650f2b613f2201514ccbf440359b91ff91b
“Sanam” <pourmand2010@users.noreply.github.com>
2021-12-16 15:43:16 +0330

    committed by script

--- Api/Admin/SlideController.cs
+++
@@ -1,8 +0,0 @@
-namespace Holism.Slide.AdminApi;
-
-public class SlideController : Controller<Post, Post>
-{
-    public override ReadBusiness<Post> ReadBusiness => new PostBusiness();
-    
-    public override Business<Post, Post> Business => new TicketBusiness();
-}
