8ad174a7e7ab5a5a4bdaf92115a5f7f85ea482cf
Saeed Nemati <sn.1361@gmail.com>
2022-02-16 16:41:53 +0330

    committed by script

--- Business/TextBusiness.cs
+++ Business/TextBusiness.cs
@@ -5,4 +5,15 @@ public class TextBusiness : Business<Text, Text>
     protected override Read<Text> Read => Repository.Text;
 
     protected override Write<Text> Write => Repository.Text;
+
+    public ExpandoObject GetKeyValues()
+    {
+        var items = GetAll();
+        dynamic result = new ExpandoObject();
+        foreach (var item in items)
+        {
+            ExpandoObjectExtensions.AddProperty(result, item.Key, item.Value);
+        }
+        return result;
+    }
 }
