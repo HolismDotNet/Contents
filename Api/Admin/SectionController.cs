55848f41fbd3ee5ba14553c66dd379d87b8a037f
Saeed Nemati <sn.1361@gmail.com>
2022-02-17 09:26:14 +0330

    committed by script

--- Api/Admin/SectionController.cs
+++ Api/Admin/SectionController.cs
@@ -3,4 +3,18 @@ namespace Contents;
 public class SectionController : ReadController<Section>
 {
     public override ReadBusiness<Section> ReadBusiness => new SectionBusiness();
+
+    [FileUploadChecker]
+    [HttpPost]
+    public Section SetImage(IFormFile file)
+    {
+        var sectionId = Request.Query["sectionId"];
+        if (sectionId.Count == 0)
+        {
+            throw new ClientException("Please provide sectionId");
+        }
+        var bytes = file.OpenReadStream().GetBytes();
+        var section = new SectionBusiness().ChangeImage(sectionId[0].ToLong(), bytes);
+        return section;
+    }
 }
--- Business/SectionBusiness.cs
+++ Business/SectionBusiness.cs
@@ -5,4 +5,23 @@ public class SectionBusiness : Business<Section, Section>
     protected override Read<Section> Read => Repository.Section;
 
     protected override Write<Section> Write => Repository.Section;
+
+    protected override void ModifyItemBeforeReturning(Section item)
+    {
+        item.RelatedItems.ImageUrl = Storage.GetImageUrl(ContainerName, item.ImageGuid ?? Guid.Empty);
+    }
+
+    public Section ChangeImage(long sectionId, byte[] bytes)
+    {
+        var section = Write.Get(sectionId);
+        if (section.ImageGuid.HasValue)
+        {
+            Storage.DeleteImage(ContainerName, section.ImageGuid.Value);
+        }
+        var nhdImage = ImageHelper.MakeImageThumbnail(Resolution.Nhd, null, bytes);
+        section.ImageGuid = Guid.NewGuid();
+        Storage.UploadImage(nhdImage.GetBytes(), section.ImageGuid.Value, ContainerName);
+        Write.Update(section);
+        return Get(sectionId);
+    }
 }
