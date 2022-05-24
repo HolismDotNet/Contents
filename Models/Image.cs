184f61899ed2bc8e93dcef3f8e2a10b558770412
Saeed Nemati <sn.1361@gmail.com>
2022-05-24 09:26:42 +0430

    committed by script

--- Models/Image.cs
+++ Models/Image.cs
@@ -13,6 +13,10 @@ public class Image : IEntity, IKey
 
     public Guid ImageGuid { get; set; }
 
+    public string RecommendedDimensions { get; set; }
+
+    public string RecommendedColorScheme { get; set; }
+
     public string Key { get; set; }
 
     public dynamic RelatedItems { get; set; }
