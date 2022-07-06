b85a9c395aaab3ecaf59e034dd8c415f5a32b793
Saeed Nemati <sn.1361@gmail.com>
2022-07-06 13:02:56 +0430

    committed by script

--- Models/Views/PageView.cs
+++ Models/Views/PageView.cs
@@ -13,8 +13,6 @@ public class PageView : IEntity, IGuid, ISlug, IParent
 
     public Guid HierarchyGuid { get; set; }
 
-    public Guid? ImageGuid { get; set; }
-
     public bool AcceptsComment { get; set; }
 
     public long HierarchyId { get; set; }
