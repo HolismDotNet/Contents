63139f21ec921a45c423f4e8880f9001cbdc726b
Saeed Nemati <sn.1361@gmail.com>
2022-07-11 15:53:52 +0430

    committed by script

--- Data/Services.sql
+++ Data/Services.sql
@@ -21,9 +21,45 @@ select Id into @servicesId
 from Contents.Sections
 where `Key` = 'services';
 
-insert ignore into Contents.Items(SectionId, Title, Subtitle, Summary, CtaLink, CtaText)
+insert ignore into Contents.Items
+(
+    SectionId, 
+    Title, 
+    Subtitle, 
+    Summary, 
+    PrimaryCtaText, 
+    PrimaryCtaLink
+)
 values
-(@servicesId, 'Boost', 'Always making you better', 'We care about your current and future status by providing more feedback to you, so that you can work on them and increase your overall state.', '/service-one', 'Read more'),
-(@servicesId, 'Comparison', 'Choose with eyes open', 'Never stop at comparing options with each other. Any option might have its benefits, and its problems. Watch carefully and choose accordingly', '/comparison', 'See how'),
-(@servicesId, 'Continuity', 'Never stop, keep going', 'Many things in life are just like waves. They come and go. But we encourage you to think of the ocean itself. It always stays. Stay!', '/continuity', 'Find techniques'),
-(@servicesId, 'Reliance', 'A true pillar', 'Trust, is one word. But its meaning is more than a thousand moments that you have experienced personally. Trust, can only be created through time.', '/reliance', 'Learn more');
+(
+    @servicesId,
+    'Boost',
+    'Always making you better',
+    'We care about your current and future status by providing more feedback to you, so that you can work on them and increase your overall state.',
+    'Read more',
+    '/service-one'
+),
+(
+    @servicesId,
+    'Comparison',
+    'Choose with eyes open',
+    'Never stop at comparing options with each other. Any option might have its benefits, and its problems. Watch carefully and choose accordingly',
+    'See how',
+    '/comparison'
+),
+(
+    @servicesId,
+    'Continuity',
+    'Never stop, keep going',
+    'Many things in life are just like waves. They come and go. But we encourage you to think of the ocean itself. It always stays. Stay!',
+    'Find techniques',
+    '/continuity'
+),
+(
+    @servicesId,
+    'Reliance',
+    'A true pillar',
+    'Trust, is one word. But its meaning is more than a thousand moments that you have experienced personally. Trust, can only be created through time.',
+    'Learn more',
+    '/reliance'
+);
