96d34681ae082a16925acc85026c75b01d58d393
Saeed Nemati <sn.1361@gmail.com>
2022-07-13 12:46:33 +0430

    committed by script

---
+++ Data/SectionConfigs/ItemsHaveSvgIcon.sql
@@ -0,0 +1,37 @@
+insert ignore into Configuration.ConfigItems
+(
+    ConfigTypeId,
+    Name,
+    Description,
+    `Key`
+)
+values
+(
+    (
+        select Id
+        from Configuration.ConfigTypes 
+        where `Key` = 'Boolean'
+    ),
+    'Items have SVG icon',
+    'If checked, users can upload SVG icons for each item of the section',
+    'ItemsHaveSvgIcon'
+);
+
+insert ignore into Configuration.EntityConfigItems
+(
+    EntityTypeGuid,
+    ConfigItemId
+)
+values
+(
+    (
+        select Guid
+        from Entities.EntityTypes
+        where Name = 'section'
+    ),
+    (
+        select Id 
+        from Configuration.ConfigItems
+        where `Key` = 'ItemsHaveSvgIcon'
+    )
+);
