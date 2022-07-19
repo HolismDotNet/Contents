ccb705998e527ffe257e7d9596c5b675a3eef548
Saeed Nemati <sn.1361@gmail.com>
2022-07-19 10:58:14 +0430

    committed by script

---
+++ Data/SectionConfigs/HasContent.sql
@@ -0,0 +1,37 @@
+insert ignore into Configuration.ConfigItems
+(
+    ConfigTypeId,
+    Name,
+    Description,
+    `Key`
+)
+values
+(
+    (
+        select Id
+        from Configuration.ConfigTypes 
+        where `Key` = 'Boolean'
+    ),
+    'Has content',
+    'Users can specify the rich content of this section.',
+    'HasContent'
+);
+
+insert ignore into Configuration.EntityConfigItems
+(
+    EntityTypeGuid,
+    ConfigItemId
+)
+values
+(
+    (
+        select Guid
+        from Entities.EntityTypes
+        where Name = 'section'
+    ),
+    (
+        select Id 
+        from Configuration.ConfigItems
+        where `Key` = 'HasContent'
+    )
+);
