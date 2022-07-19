03ea0d7b1028f7a95afd98c8afbe0b8f1b3fe964
Saeed Nemati <sn.1361@gmail.com>
2022-07-19 09:54:34 +0430

    committed by script

---
+++ Data/SectionConfigs/HasImage.sql
@@ -0,0 +1,37 @@
+insert ignore into Configuration.ConfigItems
+(
+    ConfigTypeId,
+    Name,
+    Description,
+    `Key`
+)
+values
+(
+    (
+        select Id
+        from Configuration.ConfigTypes 
+        where `Key` = 'Boolean'
+    ),
+    'Has image',
+    'Users can specify an image for this section.',
+    'HasImage'
+);
+
+insert ignore into Configuration.EntityConfigItems
+(
+    EntityTypeGuid,
+    ConfigItemId
+)
+values
+(
+    (
+        select Guid
+        from Entities.EntityTypes
+        where Name = 'section'
+    ),
+    (
+        select Id 
+        from Configuration.ConfigItems
+        where `Key` = 'HasImage'
+    )
+);
